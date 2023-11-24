639e64f57070b239926cd47803e7940de5faab33
konstantinacode <konstantina_mat@hotmail.com>
2023-11-24 19:53:50 +0200

    Screenshot-demo

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const electron = require('electron')
+
+const { app, BrowserWindow, globalShortcut } = electron
+
+let mainWindow
+
+app.on('ready', _ => {
+    mainWindow = new BrowserWindow({
+        width: 0,
+        height: 0,
+        resizeable: false,
+        frame: false
+    })
+
+    //mainWindow.openDevTools()
+
+    mainWindow.loadURL(`file://${__dirname}/capture.html`)
+
+    mainWindow.on('close', _ => {
+        mainWindow = null
+    })
+
+    globalShortcut.register('Ctrl+Alt+Cmd+D', _ => {
+        mainWindow.webContents.send('capture', app.getPath('pictures'))
+    })
+})
